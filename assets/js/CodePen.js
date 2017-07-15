c619a8b31972de73d9a5537a16e4b9524e2a6d8b
dannythian <qwerty931105@hotmail.com>
2017-07-15 16:00:42 +0800

    Add files via upload

---
+++ assets/js/CodePen.js
@@ -0,0 +1,16 @@
+﻿var CodePen = (function () {
+    $outputContainer = $(".output-container");
+
+    var fullScreen = function (isFullScreen) {
+        if (isFullScreen) {
+            $outputContainer.addClass("fullScreen");
+        }
+        else {
+            $outputContainer.removeClass("fullScreen");
+        }
+    };
+
+    return {
+        fullScreen: fullScreen
+    };
+})();
