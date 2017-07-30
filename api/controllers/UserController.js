878a384b77cf8af8aa4a242ca2486bf22e28a607
brianlee0807 <brianzhenbin@yahoo.com>
2017-07-30 14:15:04 +0800

    User Contoller

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,11 +1,32 @@
 /**
  * UserController
  *
- * @description :: Server-side logic for managing users
+ * @description :: Server-side logic for managing Users
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
 module.exports = {
+	'new':function(req,res){
+        res.view();
+     },
+    
+    create: function(req,res,next){
+        User.create( req.params.all(), function userCreated(err,User){
 
+            if(err){
+
+                console.log(err);
+                req.session.flash={
+                    err:err
+                }
+
+                return res.redirect('/User/new');
+
+            } 
+
+            res.json(User);
+            req.session.flash = {};
+        })
+    } 
 };
 
