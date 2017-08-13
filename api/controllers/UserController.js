48ddc335fff9ea1af4a66c709893266fd3edb899
brianlee0807 <brianzhenbin@yahoo.com>
2017-08-14 02:05:58 +0800

    User Dashboard Edit

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -42,6 +42,43 @@ module.exports = {
             res.json(user);
             req.session.flash = {};
         })
-    } 
+    },
+    show: function(req,res,next){
+        User.findOne(req.param('id'),function foundUser (err,user){
+            if(err) return next(err);
+            if(!user) return next();
+            res.view({
+                title:"User Input", 
+                user:user
+            });    
+        });
+        
+    },
+    index: function(req,res,next){
+        User.find(function foundUser (err, users){
+            if(err) return next(err);
+            res.view({
+                users:users
+            });
+        });
+    },
+    edit : function(req,res,next){
+        User.findOne(req.param('id'),function foundUser(err,user){
+            if(err) return next(err);
+            if(!user) return next();
+            res.view({
+                user:user
+            });
+        });
+    },
+    update: function(req,res,next){
+        User.update(req.param('id'),req.params.all(),function userUpdated(err){
+            if(err){
+                return res.redirect('/user/edit/' + req.param('id'));
+            }
+
+            res.redirect('/user/show/' + req.param('id'));
+        });
+    }
 };
 
