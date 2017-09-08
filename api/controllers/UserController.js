4f341a4babeb8652f02dbc45fef7ab9598f72d71
brianlee0807 <brianzhenbin@yahoo.com>
2017-09-09 01:17:07 +0800

    changed exec to promise

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -19,14 +19,18 @@ module.exports = {
   //To create new User with parameter
   //Brian - End
   create : function(req, res, next) {
-    User.create(req.allParams()).exec(function (err, user) {
-      if (err) {
+    User.create(req.allParams())
+    .then(function(user){
+      return res.json(user);
+    })
+    .catch(function(err){
+        if (err) {
         req.session.flash = {
           err : err
         }
         return res.redirect('/user');
       }
-      return res.json(user);
+    
     });
   },
   show : function(req, res, next) {
