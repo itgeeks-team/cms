5002ac621ef38bd20b144a2c46f8841d0f681c64
David Tan <tantan_pro@hotmail.com>
2017-10-14 17:58:37 +0800

    Resolved conflicts

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -8,12 +8,4 @@ module.exports = {
 		vm.title = "CMS - Home";
 		return response.send();
     }
-<<<<<<< HEAD
-
-
-    // AJAX
-    //
 };
-=======
-};
->>>>>>> db64e587d82dd1a6bb7f9b3b89f259ee43bbccf8
